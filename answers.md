07709ede37d0d1fcac72c9d96bab48573563b5fe
IanKreger <143121780+IanKreger@users.noreply.github.com>
2024-04-10 22:29:58 +0000

    Done

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Recitation 06
 ## Answers
 
-**Name:**_________________________
+**Name:**__Ian Kreger_______________________
 **Name:**_________________________
 
 
@@ -9,12 +9,12 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 
-- **2)**
+- **2)** Work = W(n-1) + W(n-2) + 1. O(2^n)
 
-- **3)**
+- **3)** Span = S(n-1) + 1. O(n)
 
-- **4)**
+- **4)** From the counts list the pattern of the fibonacci sequence emerges. This is because the count adds 1 for each count in the spot of n, which generates the fibonacci sequence. 
 
-- **6)**
+- **6)** The maximum number of times is exactly one time. Which makes work and span O(n).
 
-- **8)**
+- **8)** The maximum number of times is n-2 times. The work and span is O(n)
--- main.py
+++ main.py
@@ -1,27 +1,47 @@
+from typing import ParamSpecArgs
+
+
 def fib_recursive(n, counts):
     """
     Return the nth Fibonacci number. 
     counts is a list of n+1 elements, where counts[i] is incremented
     each time fib_recursive(i, counts) is called.
     """    
-    counts[n] += 1
+    if n==0:
+      return 0
+    elif n==1:
+      return 1
+    else:
+      counts[n] += 1
+      return fib_recursive(n-1,counts) + fib_recursive(n-2, counts)
     ###TODO
     pass
-    
 
     
 def fib_top_down(n, fibs):
+    if fibs[n] != -1:
+      return fibs[n]
+    elif n==0:
+      return n
+    elif n==1:
+      return 1
+    fibs[n] = fib_top_down(n-1, fibs) + fib_top_down(n-2, fibs)
+    return fibs[n]
+   
     ###TODO
     pass
 
 
 def fib_bottom_up(n):
-    ###TODO
+    if n <= 1:
+      return n
+    fib = [0] * (n+1)
+    fib[1] = 1
+    for i in range(2, n + 1):
+      fib[i] = fib[i - 1] + fib[i - 2]
+    return fib[n]
+  ###TODO
     pass
 
 
 
-
-def fib_bottom_up_better(n):
-    ###TODO
-    pass
